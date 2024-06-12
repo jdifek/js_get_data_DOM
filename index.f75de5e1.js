3b07c261cb30503da57d9245e7d95088a8cbccf8
Den <clixqqk@gmail.com>
2024-06-13 01:38:16 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const spanNumber = document.querySelectorAll(".population");
+const totalPopulation = document.querySelector(".total-population");
+const averagePopulation = document.querySelector(".average-population");
+let total = 0;
+spanNumber.forEach((num)=>{
+    total += parseInt(num.textContent);
+});
+const average = total / spanNumber.length;
+totalPopulation.textContent = total;
+averagePopulation.textContent = average.toFixed(2);
+
+//# sourceMappingURL=index.f75de5e1.js.map
---
+++ index.f8120588.js
@@ -0,0 +1,2 @@
+var t=document.querySelectorAll(".population"),e=document.querySelector(".total-population"),o=document.querySelector(".average-population"),n=0;t.forEach(function(t){n+=parseInt(t.textContent)});var r=n/t.length;e.textContent=n,o.textContent=r.toFixed(2);
+//# sourceMappingURL=index.f8120588.js.map
